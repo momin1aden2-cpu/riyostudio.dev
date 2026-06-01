4b010f910562498a1849a6190e0361876a93a31a
mumin ahmed <momin1aden2@gmail.com>
2026-06-01 17:46:18 +0800

    ui: flatten navbar links by removing 3-dot dropdown menu

---
+++ fix-script.js
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+let script = fs.readFileSync('script.js', 'utf8');
+
+// Remove dropdown logic from script.js
+const regex = /\/\/ Navigation Dropdown[\s\S]*?\}?\s*\}\);\s*\}\);/g;
+script = script.replace(regex, '');
+
+fs.writeFileSync('script.js', script, 'utf8');
